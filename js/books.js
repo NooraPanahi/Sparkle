2cf834d368e38fea15f735fd7dc0cbadb1757b06
NooraPanahi <nourap064@gmail.com>
2026-06-09 01:16:46 +0330

    feat: add initial data model

---
+++ js/books.js
@@ -0,0 +1,20 @@
+const books = [
+    {
+        id: 1,
+        title: "The Plague",
+        author: "Albert Camus",
+        rating: 0,
+        status: "Currently reading",
+        note: "a note",
+        image: "assets/thePlague.jpg"
+    },
+    {
+        id: 2,
+        title: "title",
+        author: "author",
+        rating: 0,
+        status: "status",
+        note: "a note",
+        image: "..."
+    }
+];
