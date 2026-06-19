2121a1f5c6008a08bb9916f49d91e6803532b5e6
NooraPanahi <nourap064@gmail.com>
2026-06-20 01:27:36 +0330

    feat: add more books

--- js/books.js
+++ js/books.js
@@ -1,24 +1,183 @@
 const books = [
     {
-        id: 1,
         title: "The Plague",
         author: "Albert Camus",
         genre: "Philosophy",
-        favorite: true,
+        favorite: false,
         rating: 0,
         status: "Currently reading",
         note: "a note",
-        image: "assets/thePlague.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1709092104i/439979.jpg"
     },
     {
-        id: 2,
-        title: "title",
-        author: "author",
-        genre: "genre",
+        title: "Coraline: The Graphic Novel",
+        author: "Neil Gaiman",
+        genre: "Horror fiction",
         favorite: false,
-        rating: 0,
-        status: "status",
+        rating: 8.5,
+        status: "read",
         note: "a note",
-        image: "..."
-    }
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1348347289i/1967070.jpg"
+    },
+    {
+        title: "Everything, Everything",
+        author: "Nicola Yoon",
+        genre: "Romance",
+        favorite: false,
+        rating: 8,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1450515891i/18692431.jpg"
+    },
+    {
+        title: "They Both Die at the End",
+        author: "Adam Silvera",
+        genre: "Romance",
+        favorite: false,
+        rating: 10,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1494333138i/33385229.jpg"
+    },
+    {
+        title: "Th First To Die at the End",
+        author: "Adam Silvera",
+        genre: "Romance",
+        favorite: false,
+        rating: 9,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1647651404i/59723790.jpg"
+    },
+    {
+        title: "The Little Prince",
+        author: "Antoine de Saint-Exupéry",
+        genre: "Science fantasy",
+        favorite: true,
+        rating: 10,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1367545443i/157993.jpg"
+    },
+    {
+        title: "The Only Story",
+        author: "Julian Barnes",
+        genre: "Romance",
+        favorite: false,
+        rating: 5,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1542140881i/42771492.jpg"
+    },
+    {
+        title: "Schoolgirl",
+        author: "Osamu Dazai",
+        genre: "Drama",
+        favorite: false,
+        rating: 7,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1647450251i/12483882.jpg"
+    },
+    {
+        title: "Daisy Darker",
+        author: "Alice Feeney",
+        genre: "Mystery",
+        favorite: false,
+        rating: 4,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1690904510i/195498826.jpg"
+    },
+    {
+        title: "The Damned Yard",
+        author: "Ivo Andrić",
+        genre: "Historical",
+        favorite: false,
+        rating: 7,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1675415256i/84162143.jpg"
+    },
+    {
+        title: "مثل خون در رگ های من",
+        author: "Ahmad Shamlou",
+        genre: "Poetry",
+        favorite: false,
+        rating: 10,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1432024973i/25547097.jpg"
+    },
+    {
+        title: "A Journey Around My Room",
+        author: "Xavier de Maistre",
+        genre: "Classics",
+        favorite: false,
+        rating: 9,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1557576892i/43307609.jpg"
+    },
+    {
+        title: "Dying",
+        author: "Arthur Schnitzler",
+        genre: "Classics",
+        favorite: false,
+        rating: 9,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1525192086i/40018205.jpg"
+    },
+    {
+        title: "Reunion",
+        author: "Fred Uhlman",
+        genre: "Historical Fiction",
+        favorite: false,
+        rating: 10,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1407023871i/863466.jpg"
+    },
+    {
+        title: "Pulp",
+        author: "Charles Bukowski",
+        genre: "Detective fiction",
+        favorite: false,
+        rating: 9.5,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1340203298i/6627313.jpg"
+    },
+    {
+        title: "The Suicide Shop",
+        author: "Jean Teulé",
+        genre: "Black Humor",
+        favorite: false,
+        rating: 9.5,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1737468416i/45186042.jpg"
+    },   
+    {
+        title: "We Were Liars",
+        author: "E. Lockhart",
+        genre: "Mystery Thriller",
+        favorite: false,
+        rating: 6,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1502799960i/26115894.jpg"
+    },  
+    {
+        title: "Dead Poets Society",
+        author: "Nancy Horowitz Kleinbaum",
+        genre: "Dark Academia",
+        favorite: true,
+        rating: 10,
+        status: "read",
+        note: "a note",
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1368252673i/1290733.jpg"
+    },
+
 ];
